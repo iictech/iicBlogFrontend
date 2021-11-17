cd7a1b041c7b3e0d5beb643b67ed7d9183a6d0c4
ad-rinaline2 <arindol0921@gmail.com>
2021-11-17 14:55:56 +0530

    Just testing

---
+++ src/components/HomePageContent/HomePageContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function HomePageContent{
+  return(
+    <div className="container">
+      <div className="first-img">
+        <img src=""></img>
+      </div>
+      <div className="first-content">
+        <h2> VR:How it can impact our lives</h2>
+      </div>
+
+    </div>
+  );
+}
