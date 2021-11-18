d1b375657027b19616b3483e6c71e4ffb7101adb
Shadow129-sys <gsayankr02@gmail.com>
2021-11-18 21:26:25 +0530

    Homepage Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/Home";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/NavBar/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>} />
+            </Routes>
+            <Footer/>
+        </Router>
   );
 }
 
